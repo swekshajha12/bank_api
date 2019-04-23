f67c0b2ceda6587a5b0b011c389e596735d04691
sweksha12 <jhasweksha12@gmail.com>
2019-04-24 00:32:44 +0530

    updated app.py included get and post both

--- app.py
+++ app.py
@@ -1,66 +1,84 @@
 #!/usr/bin/env python
 from flask import Flask,request,jsonify
-
-from flask_sqlalchemy import SQLAlchemy
+from fetch_res_postgresql import get_details_using_ifsc,get_details_using_bank_name_city
 app = Flask(__name__)
 
-from fetch_res_postgresql import get_details_using_ifsc,get_details_using_bank_name_city
+def check_if_get_post(url,meth):
+	if url =='details':
 
-@app.route('/ifsc',methods=["POST"])
-def hello_world():
-
-	if request.method=="POST":
-		try:
-
+		if meth=="POST":
 			data=request.get_json()
-			print(data)
+			return data['bank_name'],data['city']
+		else:
+			return request.headers['bank_name'],request.headers['city']
 
-			ifsc=data['ifsc']
-			li=["ifsc","bank_id","branch","address","city","dist","state"]
-			dc={}
-			ans=get_details_using_ifsc(ifsc)
-			# print(type(ans))
-			for i,j in enumerate(ans[0]):
-				dc[li[i]]=j
-			return jsonify(dc)
-
-		except Exception as e:
-			print(e)
+	elif url=='ifsc':
 		
-	return 'No success'	
-
-
-@app.route('/details',methods=["POST"])
-def hello():
-
-	if request.method=="POST":
-		try:
-
+		if meth=="POST":
 			data=request.get_json()
-			# print(data)
+			return data['ifsc']
+		else:
+			return request.headers['ifsc']
+				
 
-			name=data['bank_name']
-			city=data['city']
-			ans=get_details_using_bank_name_city(name,city)
 
-			li=["ifsc","bank_id","branch","address","city","dist","state"]
+@app.route('/ifsc',methods=["GET","POST"])
+def ifsc():
 
-			dc={}
-
-			for i,j in enumerate(ans):
-				dc[i]={}
-
-				for no,row_elements in enumerate(j):
-					dc[i][li[no]]=row_elements
+	
+	try:
 		
-
-			# print(type(ans))
-			return jsonify(dc)
-
-		except Exception as e:
-			print(e)
 		
-	return 'No success'
+		ifsc=check_if_get_post('ifsc',request.method)
+		li=["ifsc","bank_id","branch","address","city","dist","state"]
+		dc={}
+		ans=get_details_using_ifsc(ifsc)
+		# print(type(ans))
+
+		for i,j in enumerate(ans[0]):
+			dc[li[i]]=j
+		return jsonify(dc)
+
+	except Exception as e:
+		print(e)
+		
+		return 'No success'	
+
+
+
+@app.route('/details',methods=["GET","POST"])
+def details():
+
+	
+	try:
+
+		
+		# print(request.headers)
+		# name=request.headers['bank_name']
+		# city=request.headers['city']
+		name,city=check_if_get_post('details',request.method)
+		ans=get_details_using_bank_name_city(name,city)
+
+		li=["ifsc","bank_id","branch","address","city","dist","state"]
+
+		dc={}
+
+		for i,j in enumerate(ans):
+			dc[i]={}
+
+			for no,row_elements in enumerate(j):
+				dc[i][li[no]]=row_elements
+	
+
+		# print(type(ans))
+		return jsonify(dc)
+
+	except Exception as e:
+		print(e)
+	
+		return 'No success'	
+
+
 if __name__ == '__main__':
 	app.run(debug=True,port=8000)
 
